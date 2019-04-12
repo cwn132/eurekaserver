9861f79d5776bacddaf6fad1a0f4012c62bea3f3
cwn132 <19887460@qq.com>
2019-04-12 18:37:04 +0800

    Initial commit

---
+++ src/main/java/com/springcloud/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,21 @@
+package com.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+*访问地址 ：http://localhost:8761/
+ */
+@EnableEurekaServer //Eureka服务启动类，接受其他微服务注册进来。
+@SpringBootApplication
+public class EurekaserverApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaserverApplication.class, args);
+    }
+
+}
